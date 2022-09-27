764450f3d2a683b5657bdc0f7451610e083f6321
Brandon Wijaya <brndnwjy8@gmail.com>
2022-09-27 20:06:03 +0700

    Add material abt looping

--- forloop.js
+++ forloop.js
@@ -1,1 +1,75 @@
-// updated soon
+// F O R   L O O P
+
+// looping is used to run a block of code several times while the given condition remains true
+// the usage of loop is pretty versatile, it needs more time to fully understanding the concept
+// we are going to learn abt the first loop- FOR loop
+
+for(initialization; condition; iteration){
+    // while condition is true, run this code
+}
+
+// Note
+// FOR loop takes three different arguments separated by semicolon
+//    a. initialization, declare a random variable with a number value
+//       this will later be used in the condition
+//    b. condition, while this equals true, the code will be run
+//    c. iteration, once the code on line 8 is done. there'll be a change to the init variable,
+//       the changes caused by iteration
+//    d. while declaring a init variable, use let so the value can be changed
+
+// example 1
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+// let's start with this simple code above
+// we init a variable "i" with a value of one
+// while the value of "i" is less than equal to 10, line 23 will be run
+// whenever line 23 run, the value of "i" changed
+// the iteration is i++, which add one to "i"
+
+// breakdown :
+// a. the code will start from line 21, init a variable "i" with value of 1
+// b. validate the condition, bcs 1 <= 10 is true, line 23 will print out "1"
+// c. all code inside the curly brackets is done, iteration to "i". now the value of "i" equals 2
+// d. validate the condition, bcs 2 <= 10 is true, line 23 will print out "2"
+// e. another iteration to "i" and it keep running until "i" equals to 10
+// f. after printing "10", the iteration will make "i" have a value of 11
+//    the condition is false, so the loop stops
+
+// final output :
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+
+// example 2
+
+const limit = 5
+let result = 0
+
+for (let i = 1; i < limit; i++) {
+    result = result + i
+}
+
+console.log(result)
+
+// we can use another variable into loop like how I use limit and result inside loop
+// the loop will run as long as the value of "i" less than limit
+// while the condition is true, it will adds the value of "i" to result
+
+// breakdown : 
+// a. first run, "i" equals 1. result changed to 1 (add by 1)
+// b. second run, "i" equals 2. result changed to 2 (add by 2)
+// c. third run, "i" equals 3. result changed to 6 (add by 3)
+// d. fourth run, "i" equals 4. result changed to 10 (add by 4)
+// e. fifth run, "i" equals 5. the condition is false, the loop stops
+// f. console.log(result)
--- whileloop.js
+++ whileloop.js
@@ -1,1 +1,84 @@
-// updated soon
+// W H I L E   L O O P
+
+// another way of looping, WHILE comes with two different formats
+
+// // WHILE
+// // initialization was done outside the loop
+// while (condition) {
+//     // run this code while the condition is true
+//     // you need to add the iteration here
+// }
+
+// // DO WHILE
+// // initialization was done outside the loop
+// do {
+//     // run this code no matter what
+//     // don't forget to add the iteration here
+//     // after all code have been run, we do a validation to the condition
+//     // if true, it runs over from line 15
+//     // if false, the loop stops
+// } while (condition);
+
+// Note
+// 1. the most obvious difference between WHILE and DO WHILE is 'when to validate the condition'
+//    a. WHILE will validate the condition before it runs the code
+//    b. DO WHILE will at least runs the code once bcs the validation is done after the code
+//       so even if the condition is already false, the code from line 15 - 20 will be run
+// 2. just like the other one, init variable should be declared with let
+// 3. different from FOR loop, you can place the iteration before any other code (see example 2)
+
+// example 1
+// WHILE
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// DO WHILE
+let j = 1;
+do {
+    console.log(j)
+    j++
+} while (j <= 10);
+
+// both of these codes will print a number 1 - 10
+
+
+// example 2
+let k = 1
+while(k <= 10){
+    k++
+    console.log(k)
+}
+
+let l = 1
+while(l <= 10){
+    l++
+    console.log(l)
+    l++
+}
+
+// different from FOR, WHILE can place the iteration whenever. you can also have more than one iteration
+// just don't forget to put one, it will cause an infinity loop
+
+
+// example 3
+// WHILE
+let x = 100
+while (x <= 10) {
+    console.log("this won't be printed out")
+    console.log(x)
+    x++
+}
+
+// DO WHILE
+let y = 100
+do {
+    console.log("this will be printed out")
+    console.log(y)
+    y++
+} while (y <= 10);
+
+// both condition is false from the start but the code on line 78 - 80 will works
+// that's because on DO WHILE, the validation was done at the end
